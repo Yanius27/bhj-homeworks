b857acac94f88736761922c8c9114b8f11027489
Yanius27 <zhzh-1987@mail.ru>
2023-06-13 23:06:40 +0700

    poll done

---
+++ async-requests/poll/task.js
@@ -0,0 +1,45 @@
+const pollTitle = document.getElementById('poll__title');
+const pollAnswers = document.getElementById('poll__answers');
+
+function addPoll(obj) {
+  pollTitle.innerText = obj.data.title; 
+  for(let i = 0; i < obj.data.answers.length; i++) {
+    const btn = document.createElement('button');
+    btn.classList.add('poll__answer');
+    btn.innerText = obj.data.answers[i];
+    pollAnswers.appendChild(btn);
+    btn.addEventListener('click', () => {
+      btn.style.color = 'white';
+      alert('Спасибо, ваш голос засчитан!');
+      const resultXhr = new XMLHttpRequest();
+      resultXhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+      resultXhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      resultXhr.send(`vote=${obj.id}&answer=${i}`);
+      resultXhr.addEventListener('load', () => {
+        if(resultXhr.readyState === resultXhr.DONE) {
+          const responseStat = JSON.parse(resultXhr.response);
+          pollAnswers.innerHTML = '';
+          pollAnswers.className = 'stats';
+          for(item of responseStat.stat) {
+            const div = document.createElement('div');
+            pollAnswers.appendChild(div);
+            div.innerText = `${item.answer}: ${item.votes}%`;
+          }
+        }
+      });
+      setTimeout(() => location.reload(), 10000);
+    });
+  }
+}
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.send();
+
+xhr.addEventListener('load', () => {
+  if(xhr.readyState === xhr.DONE) {
+    const response = JSON.parse(xhr.response);
+    addPoll(response);
+  }
+});
