a48dcd79cbd603c49e1df82ed4cdf3823f6a21c4
Yanius27 <zhzh-1987@mail.ru>
2023-06-18 00:36:04 +0700

    auth fixed

---
+++ client-state/auth/task.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('signin__form');
+const formContainer = document.getElementById('signin');
+const welcome = document.getElementById('welcome');
+const spanId = document.getElementById('user_id');
+const inputs = document.querySelectorAll('input');
+const exitBtn = document.getElementById('exitBtn');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const user = localStorage.getItem('userId');
+  if(user) {
+    formContainer.classList.remove('signin_active');
+    spanId.textContent = user;
+    welcome.classList.add('welcome_active');
+    exitBtn.classList.add('exitbtn_active');
+  }
+});
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+ 
+  const formData = new FormData(form);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+  xhr.send(formData);
+
+  xhr.addEventListener('load', ()=> {
+    if(xhr.readyState === xhr.DONE && xhr.status === 201) {
+      const response = JSON.parse(xhr.response);
+      if(response.success) {
+        localStorage.setItem('userId', response['user_id']);
+        formContainer.classList.remove('signin_active');
+        spanId.textContent = response['user_id'];
+        welcome.classList.add('welcome_active');
+        exitBtn.classList.add('exitbtn_active');
+      }
+      else {
+        alert('Неверный логин/пароль');
+        inputs.forEach(el => el.value = '');
+      }
+    }
+  });
+});
+
+exitBtn.addEventListener('click', ()=> {
+  localStorage.clear();
+  location.reload();
+});
